d7e050948fb29f5923238d7dfe66732b5124e74c
jullialexandria-collab <jullialexandria@gmail.com>
2026-09-14 22:13:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -41,3 +41,40 @@ menuBtn.addEventListener("click", openMenu);
 closeBtn.addEventListener("click", closeMenu);
 
 overlay.addEventListener("click", closeMenu);
+/* ========================================
+   DROPDOWN MENU
+======================================== */
+
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach(function(dropdown) {
+
+    dropdown.addEventListener("click", function() {
+
+        const submenu = this.nextElementSibling;
+
+        dropdowns.forEach(function(otherDropdown) {
+
+            if (otherDropdown !== dropdown) {
+
+                otherDropdown.classList.remove("active");
+
+                const otherSubmenu =
+                    otherDropdown.nextElementSibling;
+
+                if (otherSubmenu) {
+                    otherSubmenu.classList.remove("active");
+                }
+            }
+
+        });
+
+        this.classList.toggle("active");
+
+        if (submenu) {
+            submenu.classList.toggle("active");
+        }
+
+    });
+
+});
